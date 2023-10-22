9adb39998e3f97dd56a065a5b4b05f13d981a2c1
Tiago Rodrigues <xtiago.rodrigues12@gmail.com>
2023-10-22 14:19:02 -0300

    clique no botão

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ function tocaSomClap () {
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.som_tecla_pom');
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-document.querySelector('.som_tecla_clap');
+document.querySelector('.tecla_clap');

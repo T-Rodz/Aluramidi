bb1566c92c7f3b875a6e51c00d15dda683a83c21
Tiago Rodrigues <xtiago.rodrigues12@gmail.com>
2023-11-21 16:18:35 -0300

    mexendo na lista

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ function tocaSomPom () {
 
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0
+
+// enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}

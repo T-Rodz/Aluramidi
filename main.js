e52d6ebc8e2154da39ee56d263b4964648ef4491
Tiago Rodrigues <xtiago.rodrigues12@gmail.com>
2023-12-05 16:56:44 -0300

    Ativando funcionamento dos sons de forma dinâmica

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 
@@ -10,10 +10,21 @@ let contador = 0
 
 // enquanto
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+    const idAudio = `#som_${instrumento}`;
+
+
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
 
     contador = contador + 1;
 
-    console.log(contador);
+    //console.log(contador);
 
 }

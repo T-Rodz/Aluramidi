c42086eeb2607239fdf40911f880afc6b4772ae6
Tiago Rodrigues <xtiago.rodrigues12@gmail.com>
2024-01-14 00:45:46 -0300

    teste para ver o erro 1

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function tocaSom (seletorAudio) {
    const elemento = document.querySelector(seletorAudio);
 
-   if (elemento != null && elemento.localname === 'audio') {
-        elemento.play()
+   if (elemento && elemento.localname === 'audio') {
+        elemento.play();
    }
    else {
     //alert('Elemento não encontrado');
-    console.log('Elemento não encontrado ou seletor inválido');
+    console.log('elemento não encontrado ou seletor inválido');
    }
 }
 

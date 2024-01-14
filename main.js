3d7754adc30eb6a709ad2c57fcf601201b9c14ce
Tiago Rodrigues <xtiago.rodrigues12@gmail.com>
2024-01-14 00:34:02 -0300

    otimização do código

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 function tocaSom (seletorAudio) {
    const elemento = document.querySelector(seletorAudio);
 
-   if (elemento === null) {
-    //alert('Elemento não encontrado');
-    console.log('Elemento não encontrado');
-   }
-   if (elemento != null) {
-    
-    if(elemento.localname === 'audio');
+   if (elemento != null && elemento.localname === 'audio') {
         elemento.play()
+   }
+   else {
+    //alert('Elemento não encontrado');
+    console.log('Elemento não encontrado ou seletor inválido');
    }
 }
 
